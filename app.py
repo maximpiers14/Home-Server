265906c7c64c283e667e0cf0c90bd0bd4c0c0d92
Maxim <maxim.piers.14@gmail.com>
2026-08-20 18:50:13 +0200

    added stats api endpoint

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify
+import psutil
 
 app = Flask(__name__)
 
 @app.route("/api/health")
 def health():
     return jsonify({"status":"ok"})
+
+@app.route("/api/stats")
+def stats(): 
+    cpu_usage = psutil.cpu_percent(interval=1)
+    ram = psutil.virtual_memory()
+    disk = psutil.disk_usage('/')
+
+    return jsonify({
+        "cpu": cpu_usage,
+        "ram_percent": ram.percent,
+        "disk_percent": disk.percent
+    })
 
 if __name__ == "__main__":
     app.run()
